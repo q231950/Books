b44d1d573b0af3b8a042dcc8f8d6262b568b3f90
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2019-10-24 19:11:47 +0200

    Re-add AuthenticationManager stub which is still used by unit tests

---
+++ LibraryCore/Sources/LibraryCore/Authentication/Authentication Manager/AuthenticationManager+Stubbed.swift
@@ -0,0 +1,31 @@
+//
+//  AuthenticationManagerUITestingStub.swift
+//  
+//
+//  Created by Martin Kim Dung-Pham on 05.07.19.
+//
+
+import Foundation
+
+extension AuthenticationManager {
+
+    /// AuthenticationManager can be stubbed for testing purposes
+    ///
+    /// - Parameter stub: the stub to set expected values on
+    ///
+    /// *Usage:*
+    /// ```
+    /// let authenticationManagerStub = AuthenticationManager.stubbed {
+    ///     $0.authenticated = true
+    ///     $0.error = nil
+    /// }
+    /// ```
+    ///
+    public static func stubbed(_ stub: ((AuthenticationManagerStub) -> Void)) -> AuthenticationManager {
+        let stubbedAuthenticationManager = AuthenticationManagerStub.init()
+
+        stub(stubbedAuthenticationManager)
+
+        return stubbedAuthenticationManager
+    }
+}
