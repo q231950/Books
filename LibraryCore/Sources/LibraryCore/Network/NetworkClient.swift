2fbbedf92cce7998eb8b45ea4fcd2cbfc9181ffc
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2019-11-12 21:13:49 +0100

    Load test stub information from process info

--- LibraryCore/Sources/LibraryCore/Network/NetworkClient.swift
+++ LibraryCore/Sources/LibraryCore/Network/NetworkClient.swift
@@ -23,7 +23,7 @@ public class NetworkClient {
         } else {
             let session: URLSession
             if ProcessInfo().isUITesting {
-                let stubbedSession = StubbornNetwork.makePersistentSession(withName: "BooksUITests_testSignIn", path: "/Users/kim/Development/Books/Books/stubs")
+                let stubbedSession = StubbornNetwork.makePersistentSession(withProcessInfo: ProcessInfo())
                 stubbedSession.recordMode = .playback
                 stubbedSession.bodyDataProcessor = SensitiveDataProcessor()
 
