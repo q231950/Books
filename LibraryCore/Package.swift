3421f88dc2d693a747f71bf0e9c648f424e23dc3
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2019-12-15 19:18:14 +0100

    Use local version of The Stubborn Network

--- LibraryCore/Package.swift
+++ LibraryCore/Package.swift
@@ -19,8 +19,9 @@ let package = Package(
             url: "https://github.com/tadija/AEXML.git",
             .upToNextMajor(from: "4.4.0")
         ),
-        .package(url: "http://github.com/q231950/the-stubborn-network.git",
-                 .branch("master"))
+        .package(path: "/Users/kim/Development/the-stubborn-network")
+//        .package(url: "http://github.com/q231950/the-stubborn-network.git",
+//                 .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
