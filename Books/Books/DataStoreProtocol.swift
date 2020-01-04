e9ee275d56e56bb7a80b61c2c75451c1f4ebd362
Martin Kim Dung-Pham <q231950@users.noreply.github.com>
2020-01-04 14:11:28 +0100

    Persistence (#14)
    * Add Data Store Protocol and Data Store
    
    * Use Data Store in App Delegate

---
+++ Books/Books/DataStoreProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  DataStoreProtocol.swift
+//  Books
+//
+//  Created by Martin Kim Dung-Pham on 13.11.19.
+//  Copyright © 2019 Martin Kim Dung-Pham. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol DataStore {
+
+    /// The persistent container of the DataStore can be used to fetch items from its contexts or create new ones
+    var persistentContainer: NSPersistentCloudKitContainer { get }
+
+    /// Saves the current state of the store
+    func saveViewContext()
+}
