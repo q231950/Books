6062d1497216f97de8eff0bf8017137e6bb5ab19
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2020-12-20 21:32:38 +0100

    Improve documentation

--- Books/BooksUITests/Support/XCUIApplication+launchOptions.swift
+++ Books/BooksUITests/Support/XCUIApplication+launchOptions.swift
@@ -9,26 +9,20 @@
 import Foundation
 import XCTest
 
-/// Launch options to apply to the launch of an `XCUIApplication`
-///
-enum LaunchOption {
-    /// This option tells the application to either playback stubbed network
-    /// requests for the given test case or record a new stub if none exists yet.
-    case stub(_ subject: StubbingSubject, `in`: XCTestCase)
-    /// This option tells the application to clean the keychain and any persisted data.
-    case clean
-}
-
-/// The subject to be stubbed.
-enum StubbingSubject {
-    /// Currently, only network requests can be stubbed in UI Tests
-    case networkRequests
-}
-
-
 extension XCUIApplication {
 
     /// Allows to apply options to the launch of an `XCUIApplication`.
+    ///
+    /// The following code will launch an `XCUIApplication` with 2 options:
+    /// 1. *stub* all network requests or record a new stub if none exists yet
+    /// 2. *clean* all keychain and persistent data when the app launches
+    ///
+    /// *Example*
+    ///
+    /// ```
+    /// app = XCUIApplication()
+    /// app.launch(options: [.stub(.networkRequests, in: self), .clean])
+    /// ```
     ///
     /// - Parameter options: the options to apply.
     func launch(options: [LaunchOption]) {
@@ -48,4 +42,20 @@ extension XCUIApplication {
 
         launch()
     }
+}
+
+/// Launch options to apply to the launch of an `XCUIApplication`
+///
+enum LaunchOption {
+    /// This option tells the application to either playback stubbed network
+    /// requests for the given test case or record a new stub if none exists yet.
+    case stub(_ subject: StubbingSubject, `in`: XCTestCase)
+    /// This option tells the application to clean the keychain and any persisted data.
+    case clean
+}
+
+/// The subject to be stubbed.
+enum StubbingSubject {
+    /// Currently, only network requests can be stubbed in UI Tests
+    case networkRequests
 }
